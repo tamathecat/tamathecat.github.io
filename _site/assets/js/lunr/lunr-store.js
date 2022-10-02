b280e1b4ddb22e80b167fbffae39c5f633f4a0ef
Tama the Cat <arkn92373@gmail.com>
2022-10-02 01:20:39 -0700

    chapter 56

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,31 +1,37 @@
 var store = [{
-        "title": "Chapter 51",
-        "excerpt":"Small hint: Tianxiu Planet has two large influential and evenly matched adversaries – the military and the church. When Xing Lou saw the words on the screen, a smile escaped his lips, and after pondering for a bit, he promptly sent a reply. —You guessed it. As expected! Heng He’s...","categories": [],
+        "title": "Qizi / 51",
+        "excerpt":"Tip: Tianxiu Planet has two large influential and evenly matched adversaries – the military and the church. When Xing Lou saw the words on the screen, a smile escaped his lips, and after pondering for a bit, he promptly sent a reply. —You guessed it. As expected! Heng He’s eyes...","categories": [],
         "tags": [],
         "url": "/qizi/51",
         "teaser": null
       },{
-        "title": "Chapter 52",
+        "title": "Qizi / 52",
         "excerpt":"Never before had any single scene shook Ying Feng as much as this one did–to the extent that after intently gazing at the person in front of him for so long, he’d even forgotten why he called out to him in the first place. It even caused Ling Xiao, who...","categories": [],
         "tags": [],
         "url": "/qizi/52",
         "teaser": null
       },{
-        "title": "Chapter 53",
+        "title": "Qizi / 53",
         "excerpt":"When Ling Xiao and Ying Feng passed by a certain dorm room, a loud noise suddenly erupted from inside. Ling Xiao instantly went on guard and stepped to the side; he even assumed a fighting posture, waiting for an enemy to burst forth. But so far, after that one cry,...","categories": [],
         "tags": [],
         "url": "/qizi/53",
         "teaser": null
       },{
-        "title": "Chapter 54",
-        "excerpt":"The author has something to say: Before, there were always readers who questioned why injected-with-Embers Ling Xiao couldn’t beat Ying Feng, yet yesterday’s injected-with-Embers Zhu Yue could beat Zhen He; this point has already been very clearly addressed in the original text. By the time Ling Xiao faced Ying Feng,...","categories": [],
+        "title": "Qizi / 54",
+        "excerpt":"The author has something to say: Before, there have been readers who questioned why injected-with-Embers Ling Xiao couldn’t defeat Ying Feng, yet yesterday’s injected-with-Embers Zhu Yue defeated Zhen He; this point has already been very clearly addressed in the original text. By the time Ling Xiao faced Ying Feng, the...","categories": [],
         "tags": [],
         "url": "/qizi/54",
         "teaser": null
       },{
-        "title": "Chapter 55",
+        "title": "Qizi / 55",
         "excerpt":"After some negotiation, the military still did not agree to Ying Feng’s request of directly reporting to the Gene Center. They had no choice but to drop Li Fei Sha off at the designated station halfway and then let the military aircraft take them to that destination under complete secrecy...","categories": [],
         "tags": [],
         "url": "/qizi/55",
+        "teaser": null
+      },{
+        "title": "Qizi / 56",
+        "excerpt":"The author has something to say: Some content is inconvenient to write in text, so the settings will be supplemented here. Qizhu’s abilities are locked by two values, affections value and spiritual strength. Spiritual Vision, spiritual strength &gt;= 5, detect Qizi’s location. Spiritual Communication, spiritual strength &gt;= 10, affections value...","categories": [],
+        "tags": [],
+        "url": "/qizi/56",
         "teaser": null
       }]
